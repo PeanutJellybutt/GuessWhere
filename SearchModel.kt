959a677932757f503748793ef2db861e69b6f2da
Woottipat Herabat <woottipat@gmail.com>
2018-11-30 22:04:43 +0700

    Delete SearchModel.kt

--- SearchModel.kt
+++
@@ -1,9 +0,0 @@
-package com.egci428.u5781070.guesswhere
-
-import ir.mirrajabi.searchdialog.core.Searchable
-
-class KeySelectModel (private val key: String?): Searchable {
-    override fun getTitle(): String {
-        return key!!
-    }
-}
